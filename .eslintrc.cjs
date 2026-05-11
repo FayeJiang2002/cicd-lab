eb66b7fffa5ae52ee4e7291545d7649a2477d47a
Faye <faye@FayedeMacBook-Air.local>
2026-05-11 12:17:02 +0800

    ci: add ESLint step, restore mikepenz test reporter
    Revert the plain-text Job Summary back to mikepenz/action-junit-report
    and add a dedicated ESLint step between Prettier and Test. ESLint
    parserOptions.project is dropped so lint works without per-file tsconfig
    inclusion.

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -5,10 +5,6 @@ module.exports = {
     es2022: true
   },
   parser: '@typescript-eslint/parser',
-  parserOptions: {
-    project: './tsconfig.json',
-    tsconfigRootDir: __dirname
-  },
   plugins: ['@typescript-eslint'],
   extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended'],
   ignorePatterns: ['dist/', 'node_modules/']
